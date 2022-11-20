fc72a39be10f2aca9bc601ec6236cb12b7636b17
Shafaque Rahman <shafaquerahman@shafaques-air.home>
2022-11-20 09:36:51 +0000

    added app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+
+
+
+app.listen(3000);
